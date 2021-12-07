09997f9e6406dff5d799eb0b14e966d5be046a14
HeiLAAS <809156882@qq.com>
2021-12-07 13:37:11 +0800

    first commit

---
+++ __init__.py
@@ -0,0 +1,61 @@
+from botoy import decorators as deco
+from botoy import GroupMsg, Action, FriendMsg
+from .define import *
+
+
+@deco.ignore_botself
+def receive_friend_msg(ctx: FriendMsg):
+    Action(ctx.CurrentQQ)
+
+
+@deco.ignore_botself
+def receive_group_msg(ctx: GroupMsg):
+    action=Action(ctx.CurrentQQ)
+    if ctx.Content in dialog():
+        result = dialog()[ctx.Content]
+        action.sendGroupText(ctx.FromGroupId, result)
+        # 指令管理
+    elif ctx.Content[0:4] == '学习对话' and len(ctx.Content) > 4:
+        try:
+            after = ctx.Content.split(' ', 2)
+            define(after[1], after[2])
+            result = '学习对话'
+            action.sendGroupText(ctx.FromGroupId, result)
+        except BaseException:
+            result = '学习对话失败，请检查格式'
+            action.sendGroupText(ctx.FromGroupId, result)
+    elif ctx.Content == '查看对话':
+        result = look_up()
+        action.sendGroupText(ctx.FromGroupId, result)
+    elif ctx.Content[0:4] == '删除对话' and len(ctx.Content) > 4:
+        after = ctx.Content.split(' ', 1)
+        flag = delete(after[1])
+        if flag:
+            result = '对话删除成功'
+            action.sendGroupText(ctx.FromGroupId, result)
+        else:
+            result = '对话删除失败，格式错误或者无该指令'
+            action.sendGroupText(ctx.FromGroupId, result)
+            # 写日记
+    elif ctx.Content == "创建日记本":
+        result = chuangjian(ctx.FromUserId)
+        action.sendGroupText(ctx.FromGroupId, result)
+    elif ctx.Content[0:3] == "写日记" and len(ctx.Content) > 3:
+        try:
+            after = ctx.Content.split(' ', 2)
+            result = xieriji(title=after[1], content=after[2], qq_num=ctx.FromUserId)
+            action.sendGroupText(ctx.FromGroupId, result)
+        except BaseException:
+            action.sendGroupText(ctx.FromGroupId, "日记写入失败，请检查格式")
+    elif ctx.Content[0:4] == "删除日记" and len(ctx.Content) > 4:
+        try:
+            after = ctx.Content.split(' ', 1)
+            result = shanchu(title=after[1], qq_num=ctx.FromUserId)
+            action.sendGroupText(ctx.FromGroupId, result)
+        except BaseException:
+            action.sendGroupText(ctx.FromGroupId, "日记删除失败，请检查格式或者日记标题")
+    elif ctx.Content == "查看日记" :
+        result = chakan(ctx.FromUserId)
+        action.sendGroupText(ctx.FromGroupId, result)
+
+
---
+++ define.py
@@ -0,0 +1,112 @@
+# -*- coding:utf-8 -*-
+# 用户：HYL
+# 日期：2021年11月19日
+import json
+talk_path='./plugins/bot_QQorder/对话.json'
+diary_path='./plugins/bot_QQorder/日记.json'
+
+# -------------指令管理-------------------
+# 读取对话
+def dialog():
+    with open(talk_path, 'r+') as file:
+        dic = json.load(file)
+        return dic
+
+
+# 定义指令
+def define(a, b):
+    new_dic = dialog()
+    with open(talk_path, 'w+') as file:
+        new_dic[a] = b
+        json.dump(new_dic, file)
+
+
+# 查看指令
+def look_up():
+    look = ''
+    for k, v in dialog().items():
+        look += k + ':' + v + '\n' + '\n'
+    return look
+
+
+# 指令删除
+def delete(a):
+    new_dic = dialog()
+    if a in new_dic:
+        with open(talk_path, 'w+') as file:
+            del new_dic[a]
+            json.dump(new_dic, file)
+        return True
+    else:
+        return False
+
+# ---------日记本功能-----------------
+def jiancha(qq_num):
+    with open(diary_path, 'r+', encoding='utf-8') as f:
+        dic = json.load(f)
+    if f"{qq_num}" not in dic:
+        flag = False  # 对象不存在
+    else:
+        flag = True
+    return flag
+
+
+def chuangjian(qq_num):
+    if not jiancha(qq_num):
+        with open(diary_path, 'r+', encoding='utf-8') as f:
+            dic = json.load(f)
+        with open(diary_path, 'w+', encoding='utf-8') as f:
+            dic[f"{qq_num}"] = {}
+            json.dump(dic, f)
+        return "日记本创建成功，快去写日记吧！"
+    else:
+        return "你已经了创建日记本，快去写日记吧！"
+
+
+def chakan(qq_num):
+    if jiancha(qq_num):
+        with open(diary_path, 'r+', encoding='utf-8') as f:
+            dic = json.load(f)
+        my_dic = dic[f"{qq_num}"]
+        content = ''
+        if my_dic:
+            for k, v in my_dic.items():
+                content += k + ':' + v + '\n' + '\n'
+        if content == '':
+            return "日记为空"
+        else:
+            return content
+    else:
+        return "日记查看失败，请先创建日记本！"
+
+
+def xieriji(content, title, qq_num):
+    if jiancha(qq_num):
+        with open(diary_path, 'r+', encoding='utf-8') as f:
+            dic = json.load(f)
+        with open(diary_path, 'w+', encoding='utf-8') as f:
+            dic[f"{qq_num}"][f"{title}"] = "记录时间为:\n" + str(datetime.datetime.now()) + "\n" + content
+            json.dump(dic, f)
+        return "日记已记录！"
+    else:
+        return "日记记录失败，请先创建日记本！"
+
+
+def shanchu(title, qq_num):
+    if jiancha(qq_num):
+        with open(diary_path, 'r+', encoding='utf-8') as f:
+            dic = json.load(f)
+            if title in dic[f"{qq_num}"]:
+                with open(diary_path, 'w+', encoding='utf-8') as f:
+                    dic[f"{qq_num}"].pop(f"{title}")
+                    json.dump(dic, f)
+                return "日记删除成功！"
+            else:
+                return "无此日记，请检查日记标题"
+
+    else:
+        return "日记删除失败，请先创建日记本！"
+
+
+#  ------------------------------------------------------
+
